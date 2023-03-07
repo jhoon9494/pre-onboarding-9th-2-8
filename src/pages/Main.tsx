81b38a3a9ca8529a45ba44c46d075b62e1f20a3a
jhoon9494 <wjdgns9406@gmail.com>
2023-03-07 21:53:19 +0900

    feat: 메인 페이지에서 여행 상품 리스트 노출

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useEffect, useState } from 'react';
 import { getAllProducts } from '@/api/products';
 import { IDetailInfo } from '@/interface/product';
+import ProductCard from '@/components/ProductCard';
+import { Grid, Heading, Center, Box } from '@chakra-ui/react';
 
 const Main = () => {
   const [productList, setProductList] = useState<IDetailInfo[]>([]);
@@ -15,7 +17,20 @@ const Main = () => {
     getProducts();
   }, [getProducts]);
 
-  return <div>main page</div>;
+  return (
+    <Box maxW="1280px" m={'auto'}>
+      <Center h="100px" mb="50">
+        <Heading as="h2" size="2xl">
+          8nbsp 추천 여행 상품
+        </Heading>
+      </Center>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6} m="4">
+        {productList.map((product) => {
+          return <ProductCard key={product.idx} product={product} />;
+        })}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Main;
