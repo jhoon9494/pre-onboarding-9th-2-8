cff6fd818f117d4b196d6f2a79dd0ac21c10ee00
jhoon9494 <wjdgns9406@gmail.com>
2023-03-07 21:37:03 +0900

    chore: chakra-ui 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from '@/Router';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
   );
 };
 
