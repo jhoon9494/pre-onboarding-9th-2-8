c7b3ff1b277e19bb0989f14774f93611b16e880f
jhoon9494 <wjdgns9406@gmail.com>
2023-03-07 21:35:41 +0900

    feat: main 페이지 router 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Main from '@/pages/Main';
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <Routes>
+      <Route path={'/main'} element={<Main />} />
+    </Routes>
+  );
 };
 
 export default Router;
